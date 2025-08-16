d94e1519cef2e6d1e9d52781a9497702e4a989c0
Oren Karaso <orenkaraso@gmail.com>
2025-08-16 23:44:36 +0300

    feat: architectural refactor and completed chart functionality

--- src/app/components/feature/currency-converter/currency-converter.component.ts
+++ src/app/components/feature/currency-converter/currency-converter.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, linkedSignal, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CurrencyService } from '../../../services/currency-service/currency.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,10 +6,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { BaseChartDirective } from 'ng2-charts';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { take } from 'rxjs';
+import { map, Subject, switchMap, tap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 import { HistoryService } from '../../../services/history-service/history.service';
-import { Chart, ChartData, ChartOptions, registerables } from 'chart.js';
+import { Chart, ChartOptions, registerables } from 'chart.js';
 import { ChartService } from '../../../services/chart-service/chart.service';
 
 @Component({
@@ -18,83 +19,68 @@ import { ChartService } from '../../../services/chart-service/chart.service';
   templateUrl: './currency-converter.component.html',
   styleUrl: './currency-converter.component.scss'
 })
-export class CurrencyConverterComponent implements OnInit {
-  public form: FormGroup | null = null;
-  public fromCurrencies = linkedSignal(() => this.currencies());
-  public toCurrencies = linkedSignal(() => this.currencies());
-  protected calculatedRate = 0;
-  protected isFetchingRate = signal<boolean>(false);
-  protected chartData = signal<ChartData<'line'>>({
-    labels: [],
-    datasets: []
+export class CurrencyConverterComponent {
+  private fb = inject(FormBuilder);
+  public form: FormGroup = this.fb.group({
+    amount: [1, [Validators.required, Validators.min(0.01)]],
+    fromCurrency: ['', Validators.required],
+    toCurrency: ['', Validators.required]
   });
+
+  private selectedFrom = toSignal(this.form.controls['fromCurrency'].valueChanges, { initialValue: '' });
+  private selectedTo = toSignal(this.form.controls['toCurrency'].valueChanges, { initialValue: '' });
+
+  protected fromCurrencies = computed(() => this.currencies().filter(c => c.code !== this.selectedTo()));
+  protected toCurrencies = computed(() => this.currencies().filter(c => c.code !== this.selectedFrom()));
+
+  private getRateFromApi$ = new Subject<void>();
+  private getRatesPerLastWeek$ = new Subject<void>();
+
+  protected chartData = toSignal(
+    this.getRatesPerLastWeek$.pipe(
+      switchMap(() => {
+        const { fromCurrency, toCurrency } = this.form.getRawValue();
+        return this.historyService.getRatesPerLastWeek(fromCurrency, toCurrency)
+      }),
+      map(() => this.chartService.buildChartData()),
+    ));
+
+  protected calculatedRate = toSignal(
+    this.getRateFromApi$.pipe(
+      tap(() => {
+        this.form.disable();
+        this.isFetchingRate.set(true);
+      }),
+      switchMap(() => {
+        const { fromCurrency, toCurrency } = this.form.getRawValue();
+        return this.currencyService.getRateFromApi(fromCurrency!, toCurrency!);
+      }),
+      map(rate => rate * this.form.get('amount')?.value!),
+      tap(() => {
+        this.form.enable();
+        this.form.markAsUntouched();
+        this.isFetchingRate.set(false);
+      })
+    ),
+    { initialValue: 0 }
+  );
+
+  protected isFetchingRate = signal<boolean>(false);
   public lineChartOptions: ChartOptions<'line'> = { responsive: true };
   private historyService = inject(HistoryService);
   private currencyService = inject(CurrencyService);
   private chartService = inject(ChartService);
-  private fb = inject(FormBuilder);
   private currencies = this.currencyService.currencies;
 
   constructor() {
     Chart.register(...registerables);
   }
 
-  ngOnInit(): void {
-    this.initForm();
-  }
-
-  private initForm(): void {
-    this.form = this.fb.group({
-      amount: [0, [Validators.required, Validators.min(0.01)]],
-      fromCurrency: [null, Validators.required],
-      toCurrency: [null, Validators.required]
-    });
-
-    this.setupValuechanges();
-  }
-
-  private setupValuechanges(): void {
-    this.form?.get('fromCurrency')?.valueChanges.subscribe((fromCurrency) => {
-      this.toCurrencies.update(() => this.currencies().filter(currency => currency.code !== fromCurrency));
-    });
-
-    this.form?.get('toCurrency')?.valueChanges.subscribe((toCurrency) => {
-      this.fromCurrencies.update(() => this.currencies().filter(currency => currency.code !== toCurrency));
-    });
-  }
-
-  getRate(): void {
-    this.fetchingData();
-    const values = this.form?.value;
-    this.updateCalculatedRate(values.fromCurrency, values.toCurrency, values.amount);
+  getRate() {
+    this.getRateFromApi$.next();
   }
 
   showChart() {
-    const values = this.form?.value;
-    this.historyService.getRatesPerLastWeek(values.fromCurrency, values.toCurrency).pipe(take(1)).subscribe(() => {
-      this.updateChartData();
-    });
-  }
-
-  private fetchingData() {
-    this.isFetchingRate.set(true);
-    this.form?.disable();
-  }
-
-  private updateCalculatedRate(fromCurrency: any, toCurrency: any, amount: any) {
-    this.currencyService.getRateFromApi(fromCurrency, toCurrency).pipe(take(1)).subscribe(rate => {
-      this.calculatedRate = rate * amount;
-      this.dataFetched();
-    });
-  }
-
-  private dataFetched() {
-    this.isFetchingRate.set(false);
-    this.form?.markAsUntouched();
-    this.form?.enable();
-  }
-
-  private updateChartData() {
-    this.chartData.set(this.chartService.buildChartData());
+    this.getRatesPerLastWeek$.next();
   }
 }
--- src/app/services/chart-service/chart.service.ts
+++ src/app/services/chart-service/chart.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { ChartData, ChartOptions } from 'chart.js';
+import { ChartData } from 'chart.js';
 import { HistoryService } from '../history-service/history.service';
 
 interface EmptyLineChart {
@@ -33,7 +33,7 @@ export class ChartService {
         return {
             labels: labels,
             datasets: [{
-                label: 'Exchange Rate History of Past Week',
+                label: `Exchange rate history of past week (per 1 ${ratesFromLastWeek.base})`,
                 data: rates,
                 fill: true,
                 tension: 0.3,
@@ -42,29 +42,4 @@ export class ChartService {
             }]
         };
     }
-
-    public lineChartData: ChartData<'line'> = {
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        datasets: [
-            {
-                data: [1.072, 1.075, 1.073, 1.078, 1.076, 1.081, 1.080], // Example EUR to USD rates
-                label: 'EUR to USD Exchange Rate',
-                fill: true, // Fills the area under the line
-                tension: 0.3, // Makes the line slightly curved
-                borderColor: 'rgba(75,192,192,1)',
-                backgroundColor: 'rgba(75,192,192,0.2)',
-            },
-        ],
-    };
-
-    // Chart Options
-    public lineChartOptions: ChartOptions<'line'> = {
-        responsive: true,
-        plugins: {
-            title: {
-                display: true,
-                text: 'EUR to USD Exchange Rate - Last 7 Days', // Chart title
-            },
-        },
-    };
 }
